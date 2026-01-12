656fcb9a993b75095182c912f5a6ea88683e0e3b
Guilherme Bley <guilhermecbley@gmail.com>
2026-01-11 22:45:42 -0300

    feat - changing direction

--- src/top-interview-150/candy.py
+++ src/top-interview-150/candy.py
@@ -35,33 +35,36 @@ class Solution():
         :type ratings: List[int]
         :rtype: int
         """
-        candyQtt = 0
         totalRatings = len(ratings)
         currentCandyQuantity = 1
         leftSimblingCandyQuantity = { "quantity": 1, "rate": -1 }
         rightSimblingCandyQuantity = { "quantity": 1, "rate": -1 }
+        ranges = [range(0, totalRatings - 1), reversed(range(0, totalRatings - 1))]
+        answer = []
 
-        for i in range(0, totalRatings):
-            currentCandyQuantity = 1
-            currentRate = ratings[i]
+        for rangeSearch in ranges:
+            for i in rangeSearch:
+                currentCandyQuantity = 1
+                currentRate = ratings[i]
 
-            if (i < totalRatings - 1):
-                rightSimblingCandyQuantity = { "quantity": 1, "rate": ratings[i + 1] }
+                if (i < totalRatings - 1):
+                    rightSimblingCandyQuantity = { "quantity": 1, "rate": ratings[i + 1] }
 
-            try:
-                if (leftSimblingCandyQuantity["rate"] >= 0 and currentRate > leftSimblingCandyQuantity["rate"]): # is current rating bigger than the previous
-                    currentCandyQuantity=leftSimblingCandyQuantity["quantity"] + 1
+                try:
+                    if (leftSimblingCandyQuantity["rate"] >= 0 and currentRate > leftSimblingCandyQuantity["rate"]): # is current rating bigger than the previous
+                        currentCandyQuantity=leftSimblingCandyQuantity["quantity"] + 1
 
-                elif (currentRate > rightSimblingCandyQuantity["rate"]): # is current rating bigger than the next
-                    currentCandyQuantity=rightSimblingCandyQuantity["quantity"] + 1
-
-                if (leftSimblingCandyQuantity['rate'] > currentRate and leftSimblingCandyQuantity['quantity'] <= currentCandyQuantity):
-                    currentCandyQuantity+=1
-            finally:
-                candyQtt+=currentCandyQuantity
-                leftSimblingCandyQuantity = { "quantity": currentCandyQuantity, "rate": currentRate }
+                    elif (currentRate > rightSimblingCandyQuantity["rate"]): # is current rating bigger than the next
+                        currentCandyQuantity=rightSimblingCandyQuantity["quantity"] + 1
+                finally:
+                    if (len(answer) > i and i != 0):
+                        answer[i] = max(answer[i], currentCandyQuantity)
+                    else:
+                        answer.insert(i, currentCandyQuantity)
+                    leftSimblingCandyQuantity = { "quantity": currentCandyQuantity, "rate": currentRate }
         
-        return candyQtt
+        print(answer)
+        return sum(answer)
 
             
 
@@ -71,3 +74,4 @@ print('' + str(Solution().candy([1,2,2])) + ' Expect: 4')
 print('' + str(Solution().candy([1,3,2,2,1])) + ' Expect: 7 Because => 1 + 2 + 1 + 2 + 1')
 print('' + str(Solution().candy([1,2,87,87,87,2,1])) + ' Expect: 13 Because => 1 + 2 + 3 + 1 + 3 + 2 + 1')
 print('' + str(Solution().candy([29,51,87,87,72,12])) + ' Expect: 12 Because => 1 + 2 + 3 + 2 + 2 + 1')
+print('' + str(Solution().candy([1,6,10,8,7,3,2])) + ' Expect: 18 Because => 1 + 2 + 5 + 4 + 3 + 2 + 1')
