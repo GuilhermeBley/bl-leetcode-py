3b9ae226b7a15716cd44da5fb05e9490adda258a
Guilherme Bley <guilhermecbley@gmail.com>
2026-01-31 23:11:37 -0300

    roman exercise

--- src/top-interview-150/roman-to-integer.py
+++ src/top-interview-150/roman-to-integer.py
@@ -46,11 +46,37 @@ It is guaranteed that s is a valid roman numeral in the range [1, 3999].
 """
 
 class Solution():
-    def romanToInt(self, s):
+    ROMAN = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000,
+    }
+
+    def romanToInt(cls, s):
         """
         :type s: str
         :rtype: int
         """
+        if (type(s) != str): return 0
+
+        total = len(s)
+        sum = 0
+        for i in range(0, total):
+            current = cls.ROMAN.get(s[i], 0)
+            next = 0 if i == total -1 else cls.ROMAN.get(s[i], 0)
+
+            if (next > current):
+                i+=1 # skip next
+                sum += next - current
+                continue
+
+            sum += current
+        
+        return sum
 
 print('Expecting: 3, Actual: ' + str(Solution().romanToInt('III')))
 print('Expecting: 58, Actual: ' + str(Solution().romanToInt('LVIII')))
