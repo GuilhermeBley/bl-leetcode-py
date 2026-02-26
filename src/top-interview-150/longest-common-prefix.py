d0d7483528ff24c024eb35d25c12832a9bcb3700
Guilherme Bley <guilhermecbley@gmail.com>
2026-02-25 22:27:05 -0300

    fixing max

--- src/top-interview-150/longest-common-prefix.py
+++ src/top-interview-150/longest-common-prefix.py
@@ -31,27 +31,31 @@ class Solution():
         if len(strs) == 0: return ""
 
         dic = { }
+        i = 0
+        last_max_qtt = 0
         while  (True): # going through each letter
-            i = 0
             for strc in strs:
-                if i < len(strc) and (i == 0 or strc in dic):
+                if i < len(strc):
                     key = "".join(strc)[0:i+1]
                     dic[key] = dic.get(key, 0) + 1
             i+=1
             # Removing low quantities
-            maxQtt = max(qtt for qtt in dic.items())
+            maxQtt = max(qtt for _,qtt in dic.items())
+            if maxQtt == last_max_qtt:
+                break
+            last_max_qtt = maxQtt
             dic = { letter: qtt for letter, qtt in dic.items() if qtt == maxQtt }
 
             if (len(dic) in [0, 1]):
                 break
 
         if (len(dic) == 1):
-            return dic.values()[0]
+            return list(dic.keys())[0]
         
         return ""
 
 
-print("['dog','racecar','car'] = '' and current is '{val}'".format(val=Solution().longestCommonPrefix(["dog","racecar","car"])))
-print("['flower','flow','flight'] = 'fl' and current is '{val}'".format(val=Solution().longestCommonPrefix(["flower","flow","flight"])))
+#print("['dog','racecar','car'] = '' and current is '{val}'".format(val=Solution().longestCommonPrefix(["dog","racecar","car"])))
+#print("['flower','flow','flight'] = 'fl' and current is '{val}'".format(val=Solution().longestCommonPrefix(["flower","flow","flight"])))
 print("['gol','gol bola', 'flower','flow','flight'] = 'flo' and current is '{val}'".format(
     val=Solution().longestCommonPrefix(["gol", "gol bola", "flower","flow","floght"])))
