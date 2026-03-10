4cbfc1015ca828e05073f931542c81dc8fb5ed6d
Guilherme Bley <guilhermecbley@gmail.com>
2026-03-09 23:29:21 -0300

    feat - first try

--- src/top-interview-150/zigzag-conversion.py
+++ src/top-interview-150/zigzag-conversion.py
@@ -21,7 +21,7 @@ Output: "PAHNAPLSIIGYIR"
 0   1   2   3   4   5   6   7   8   9   10  11  12  13
 P   A   Y   P   A   L   I   S   H   I   R   I   N   G
 Index walk: 
-0 -> 4 -> 8 -> 12 -> (+4)
+0 -> 4 -> 8 -> 12 -> (numRows * 2 - 2 = 4)
 1 -> 3 -> 5 -> 7 -> 9 -> 11 -> 13 (+2)
 2 -> 6 -> 10 (+4)
 
@@ -38,26 +38,58 @@ P     I
 P   A   Y   P   A   L   I   S   H   I   R   I   N   G
 s   s   s   s   s   s   s   s   s   x   s   s   s   x
 Index walk: 
-0 -> 6 -> 12 (+6) (numRows + 1)
+0 -> 6 -> 12 (+6) (numRows * 2 - 2 = 6)
 1 -> 5 -> 7 -> 11 -> 13 (+4, +2, ...) (numRows + )
 2 -> 4 -> 8 -> 10 (+2, +4, ...)
 3 -> 9 (+6)
 
 Example 3:
+Input: s = "PAYPALISHIRING", numRows = 5
+Output: "PINALSIGYAHRPI"
+Explanation:
+P       H 
+A     S   I
+Y   I       R
+P L           I   G
+A               N
+
+0   1   2   3   4   5   6   7   8   9   10  11  12  13
+P   A   Y   P   A   L   I   S   H   I   R   I   N   G
+x   x   x   x   s   x   x   x   x   x   x   x   s   s
+Index walk: 
+0 -> 8 (numRows * 2 - 2 = 6)
+1 -> 7 -> 9 -> 
+2 -> 6 -> 10 ->
+3 -> 5 -> 11 -> 13
+4 -> 12 (numRows * 2 - 2 = 6)
+
+Example 4:
 Input: s = "A", numRows = 1
 Output: "A"
 """
 
 class Solution(object):
+
+    def converToZigZagIndex(self, currentIndex, totalText, numRows) -> int:
+        lastIndex = totalText-1
+        if (lastIndex < 1): return 0
+
+        if (currentIndex == 0): return 0
+
+        return numRows * 2 - currentIndex + 1
+
     def convert(self, s, numRows):
         """
         :type s: str
         :type numRows: int
         :rtype: str
         """
-        newS = []
+        newS = ""
+        total_length = len(s)
+        for i in range(0, total_length):
+            newS += s[self.converToZigZagIndex(currentIndex=i, numRows=numRows, totalText=total_length)]
 
-        return ''.join(newS)
+        return newS
         
         
 print(Solution().convert("PAYPALISHIRING", 3))
